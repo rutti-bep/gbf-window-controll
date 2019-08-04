cb5a1d3020a048ca97157a3249aeeda5ab794a04
rutti-bep <s.konnno.train233@gmail.com>
2019-08-04 18:25:27 +0900

    add gbf window controll

--- dest/js/background.js
+++ dest/js/background.js
@@ -1,1 +1,38 @@
-"";
+var GBFWindowObjectId;
+var ToolsWindowObjectReference;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var response = {"status":true};
+  switch (request.method){
+    case "gbfOpen":
+      if(GBFWindowObjectId == null){
+        chrome.windows.create({url:request.url,type:"popup"},
+            (_window)=>{
+              GBFWindowObjectId  = _window.id;
+              console.log(GBFWindowObjectId);
+            });
+        chrome.windows.onRemoved.addListener((GBFWindowObjectId)=>{
+          GBFWindowObjectId=null;
+        });
+      }else{
+        chrome.windows.get(GBFWindowObjectId,{populate:true},(_window)=>{
+          //console.log(_window);
+          if (request.url !== _window.tabs[0].url){
+            chrome.tabs.update(_window.tabs[0].id,{url:request.url});
+          }else{
+            chrome.windows.update(GBFWindowObjectId,{focused:true});
+          }
+        });
+      }
+      break;
+    case "toolsOpen":
+      if(ToolsWindowObjectReference == null || ToolsWindowObjectReference.closed){
+        ToolsWindowObjectReference = window.open("dest/tools.html","つーる","status=yes");
+      }else{
+        ToolsWindowObjectReference.focus();
+      } 
+      break;
+  };
+  sendResponse(response);
+});
+
--- dest/js/popup.js
+++ dest/js/popup.js
@@ -1,4 +1,13 @@
 $(function() {
-  var gbfPopupButton = $("gbf-popup-button");
-  var toolsPopupButton = $("tools-popup-button");
+  var gbfPopupButton = $("#gbf-popup-button");
+  var toolsPopupButton = $("#tools-popup-button");
+
+  gbfPopupButton.click(()=>{
+    chrome.runtime.sendMessage({"method":"gbfOpen","url":"https://twitter.com/home"}); 
+  });
+
+  toolsPopupButton.click(()=>{
+    chrome.runtime.sendMessage({"method":"toolsOpen"}); 
+  });
+
 })
